2f514bec238e993f172191db5f09f60c6ca5ac9d
DRH <dragonfourtyseven@gmail.com>
2023-08-07 12:30:32 +0500

    init ftms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from "./components/gettingGroupTabs";
+import NewTournamentForm from "./components/ftms";
+import {Box, Component} from "./components/new";
+import Testing from "./components/testing";
+import Figma from "./components/figma";
+import Index from "./components";
+import Tournaments from "./components/tournaments";
+import Matches from "./components/matches";
+import ClubsDetails from "./components/teams";
+import TournamentDetails from "./components/tournamentGroups";
+import MatchesNew from "./components/matchesNewDesign";
+import YourComponent from "./components/formatingDateTime";
+import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import Skeletontest from "./components/skeletontest";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<TournamentDetails/>}/>
+                <Route path="/teams" element={<ClubsDetails/>}/>
+                <Route path="/new" element={<NewTournamentForm/>}/>
+                <Route path="/tabs" element={<Example/>}/>
+                <Route path="/skel" element={<Skeletontest/>}/>
+            </Routes>
+        </Router>
+
+
+    );
 }
 
 export default App;
